c8c3979a18b54d695a47597433dfdb2d767694bb
dalfior <aecio.dalfior@gmail.com>
2022-07-06 18:47:17 -0300

    Ionic - Aplicativo Task

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { TaskService } from './../services/task.service';
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController, 
+              public taskService: TaskService,
+              public toastController: ToastController) {}
 
+  async presentAlertPromptAdd() {
+    const alert = await this.alertController.create({
+      //cssClass: 'my-custom-class',
+      header: 'Adicionar Tarefa!',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'Tarefa'
+        },
+        // input date with min & max
+        {
+          name: 'date',
+          type: 'date',
+          min: '2020-01-01',
+          max: '2025-01-31'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        }, {
+          text: 'Salvar',
+          handler: (alertData) => {
+            if (alertData.task != "")
+            this.taskService.addTask(alertData.task, alertData.date);
+            else {
+              this.presentToast();
+              this.presentAlertPromptAdd();
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentToast(){
+    const toast = await this.toastController.create({
+      message: "Preencha a tarefa!",
+      duration: 2000
+    });
+    toast.present();
+  }
 }
