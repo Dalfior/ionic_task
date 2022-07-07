c17558051dc5ce24800774979843cef1015c416b
dalfior <aecio.dalfior@gmail.com>
2022-07-07 15:48:05 -0300

    Aula V - Ionic MOOC IFES

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -51,6 +51,64 @@ export class HomePage {
     await alert.present();
   }
 
+  async presentAlertPromptDelete(index: number) {
+    const alert = await this.alertController.create({
+      //cssClass: 'my-custom-class',
+      header: 'Excluir Tarefa!',
+      message: 'Deseja realmente a tarefa?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        }, {
+          text: 'Excluir',
+          handler: () => this.taskService.delTask(index)
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentAlertPromptUpdate(index: number, task) {
+    const alert = await this.alertController.create({
+      //cssClass: 'my-custom-class',
+      header: 'Atualizar Tarefa!',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'Tarefa',
+          value: task.value
+        },
+        // input date with min & max
+        {
+          name: 'date',
+          type: 'date',
+          min: '2020-01-01',
+          max: '2025-01-31',
+          value: task.date.getFullYear() + "-" + ((task.date.getMonth()+1) < 10 ? "0" + task.date.getMonth()+1 : task.date.getMonth()+1) + "-" + ((task.date.getDay()+1) < 10 ? "0" + task.date.getDay() : task.date.getDay())
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        }, {
+          text: 'Salvar',
+          handler: (alertData) => {
+            if (alertData.task != "")
+              this.taskService.updateTask(index, alertData.task, alertData.date);
+            else {
+              this.presentToast();
+              this.taskService.updateTask(index, alertData.task, alertData.date);
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
   async presentToast(){
     const toast = await this.toastController.create({
       message: "Preencha a tarefa!",
